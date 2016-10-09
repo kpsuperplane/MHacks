da4c2c61bcc598cf1f520a86bd5f7db5520b4aa5
Kevin Pei <kevin-pei@hotmail.com>
2016-10-08 23:01:54 -0400

    Update

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 from src import generator, editor
 
-<<<<<<< HEAD
-editor.combineAudio(generator.generateFromText("Tomorrow and tomorrow and tomorrow Creeps in this petty pace from day to day To the last syllable of recorded time And all our yesterdays have lighted fools The way to dusty death Out out brief candle Life is but a walking shadow a poor player That struts and frets his hour upon the stage And then is heard no more It is a tale Told by an idiot full of sound and fury Signifying nothing", "test/trump.pickle"), "test.wav")
-=======
-editor.combineAudio(generator.generateFromText("Did you know that the american population is pretty beautiful", "test/trump.pickle"), "test.wav")
->>>>>>> 9221e157dbc975ce3657b9879dd60f06039f1868
+editor.combineAudio(generator.generateFromText("The vocabulary of this sentence contains an abundance of rarely used words phrases and can paraphrase sentences in a variety of ways that are chosen randomly", "speech/trump.pickle"), "test.wav")
